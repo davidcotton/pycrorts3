e79a812ef2fdd9df609445d40014df2684dda7e3
dave <davidcotton@users.noreply.github.com>
2019-10-14 15:57:30 +1100

    Add `untangle` to dependencies

--- setup.py
+++ setup.py
@@ -5,5 +5,5 @@ setup(
     version='0.1.0',
     description='PycroRTS 3',
     long_description=open('README.md').read(),
-    install_requires=['gym>=0.10.3', 'ray[rllib, debug]'],
+    install_requires=['gym>=0.10.3', 'ray[rllib, debug]', 'untangle'],
 )
