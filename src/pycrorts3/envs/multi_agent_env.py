Fixed up bugs around dying units and pending actions

src/pycrorts3/envs/multi_agent_env.py
@@ -31,7 +31,7 @@ class PycroRts3MultiAgentEnv(MultiAgentEnv):
             # }),
             'player_id': spaces.Box(low=0, high=1, shape=(1,), dtype=np.uint8),
             'resources': spaces.Box(low=0, high=np.iinfo('uint16').max, shape=(1,), dtype=np.uint16),
-            'time': spaces.Box(low=0, high=np.iinfo('uint8').max, shape=(1,), dtype=np.uint8),
+            'time': spaces.Box(low=0, high=np.iinfo('uint16').max, shape=(1,), dtype=np.uint16),
         })
 
     def reset(self):
@@ -40,7 +40,7 @@ class PycroRts3MultiAgentEnv(MultiAgentEnv):
         for unit_id, unit in self.game.map.units.items():
             player_id = unit.player_id
             obs = {
-                'action_mask': self.game.map.get_action_mask(unit),
+                'action_mask': self.game.get_action_mask(unit),
                 'board': np.ravel(self.game.get_state(unit_id)),
                 'player_id': np.array([player_id]),
                 'resources': np.array([self.game.map.players[player_id].minerals]),
@@ -81,7 +81,7 @@ class PycroRts3MultiAgentEnv(MultiAgentEnv):
         for unit_id, unit in self.game.map.units.items():
             player_id = unit.player_id
             obs = {
-                'action_mask': self.game.map.get_action_mask(unit),
+                'action_mask': self.game.get_action_mask(unit),
                 'board': np.ravel(self.game.get_state(unit_id)),
                 'player_id': np.array([player_id]),
                 'resources': np.array([self.game.map.players[player_id].minerals]),

src/pycrorts3/game/game.py
@@ -1,15 +1,20 @@
 from collections import defaultdict, deque
+import itertools
 from typing import List
 
-from .actions import Action, NoopAction, MoveAction, AttackAction
+import numpy as np
+
+from .actions import Action, NoopAction, MoveAction, AttackAction, ActionEncodings
 from .map import Map
 from .player import Player
+from ..game.position import cardinal_to_euclidean
+from .units import Unit
 
 MAP_FILENAME = '4x4_melee_light2.xml'
 MAX_STEPS_PER_GAME = 1500
 REWARD_WIN = 1.0
-REWARD_DRAW = 0.5
-REWARD_LOSE = 0.0
+REWARD_DRAW = 0.0
+REWARD_LOSE = -1.0
 REWARD_STEP = 0.0
 UTT_VERSION = 2
 
@@ -34,11 +39,10 @@ class Game:
         self.time = 0
         self.is_game_over = False
         self.winner = None
-        self.pending_actions = deque()
-        self.pending_positions = set()
+        self.pending_actions: deque[Action] = deque()
 
         # step state
-        self.queued_actions = deque()
+        self.queued_actions: deque[Action] = deque()
 
     def reset(self):
         self.map = Map(self.map_filename())
@@ -46,18 +50,57 @@ class Game:
         self.is_game_over = False
         self.winner = None
         self.pending_actions.clear()
-        self.pending_positions.clear()
         self.queued_actions.clear()
 
     def step(self, action):
         # validate action, if invalid replace with NOOP action with same duration
         #  - check terrain & unit positions
         #  - check pending actions
-        end_pos = action.position
-        if not self.map.is_legal_action(action) or end_pos in self.pending_positions:
+        if not self.is_legal_action(action):
             unit = self.map.get_unit(action.unit_id)
             action = NoopAction(action.unit_id, unit.position, action.start_time, action.end_time)
+        # queue the action to be processed at the end of turn
         self.queued_actions.append(action)
+
+    def is_legal_action(self, action):
+        future_actions = list(itertools.chain(*self.pending_actions))
+        future_actions += self.queued_actions
+        for other in future_actions:
+            if action.unit_id == other.unit_id:
+                return False  # an action already in-progress for this unit
+            if isinstance(action, MoveAction):
+                if action.position == other.position:
+                    return False  # square _might_ be occupied when the action executes (copying microRTS logic)
+        is_valid = self.map.is_legal_action(action)
+        return is_valid
+
+    def get_action_mask(self, unit: Unit):
+        if unit.is_dead():
+            action_mask = np.zeros(shape=(len(ActionEncodings),), dtype=np.uint8)
+            action_mask[0] = 1
+            return action_mask
+
+        future_actions = list(itertools.chain(*self.pending_actions))
+        future_actions += self.queued_actions
+        for action in future_actions:
+            if action.unit_id == unit.id:
+                # target unit has an action in-progress, can't make any legal actions
+                action_mask = np.zeros(shape=(len(ActionEncodings),), dtype=np.uint8)
+                action_mask[0] = 1  # use NOOP until we add in-progress flag to action mask
+                break
+        else:
+            action_mask = self.map.get_action_mask(unit)
+            # mask move directions set to be occupied by other pending actions
+            for action_id in range(1, 5):
+                if action_mask[action_id] == 0:
+                    continue
+                action_type = ActionEncodings(action_id).name
+                position = cardinal_to_euclidean(unit.position, action_type)
+                for other in future_actions:
+                    if position == other.position:
+                        action_mask[action_id] = 0
+
+        return action_mask
 
     def update(self) -> None:
         assert not self.is_game_over
@@ -77,20 +120,16 @@ class Game:
         # 2) move queued actions (this step) to pending (future steps)
         while len(self.queued_actions):
             action = self.queued_actions.popleft()
-            end_time = action.end_time
-            while len(self.pending_actions) <= end_time:
+            relative_end_time = action.end_time - self.time  # relative from now
+            while len(self.pending_actions) <= relative_end_time:
                 self.pending_actions.append([])
-            if action.position in self.pending_positions:
-                raise ValueError
-            assert action.position not in self.pending_positions
-            time = end_time - self.time
-            self.pending_actions[time].append(action)
-            self.pending_positions.add(action.position)
+            self.pending_actions[relative_end_time].append(action)
             self.map.get_unit(action.unit_id).in_progress = True
 
         # 3) execute actions that complete this step
         to_execute = self.pending_actions.popleft()
-        for action in to_execute:
+        while len(to_execute):
+            action = to_execute.pop()
             if isinstance(action, NoopAction):
                 pass
             elif isinstance(action, MoveAction):
@@ -98,19 +137,36 @@ class Game:
             elif isinstance(action, AttackAction):
                 dead_unit = self.map.attack_unit(action.unit_id, action.position)
                 if dead_unit:
-                    # copy the microRTS reference implementation
+                    # copy microRTS logic
                     # if two units attack simultaneously, the first unit kills the 2nd, before 2nd strikes
-                    for step in self.pending_actions:
-                        for a in step:
-                            if a.unit_id == dead_unit.id:
-                                self.pending_positions.remove(a.position)
-                    self.map.remove_unit(dead_unit)
-            self.pending_positions.remove(action.position)
+                    for i, step_actions in enumerate(self.pending_actions):
+                        for j, pending_action in enumerate(step_actions):
+                            if pending_action.unit_id == dead_unit.id:
+                                self.pending_actions[i].pop(j)
+                                break
+                        else:
+                            continue
+                        break
+                    for i, pending_action in enumerate(to_execute):
+                        if pending_action.unit_id == dead_unit.id:
+                            if pending_action.unit_id == dead_unit.id:
+                                to_execute.pop(i)
+                                break
+                    self.map.remove_unit(dead_unit.id)
+                    # check player has units
+                    num_units = sum(1 if u.player_id == dead_unit.player_id else 0 for u in self.map.units.values() if
+                                    not u.is_dead())
+                    if num_units == 0:
+                        self.is_game_over = True
+                        self.winner = 1 - dead_unit.player_id
+                        print('GAME OVER, winner: %s' % self.winner)
+                        return  # abort updating, game over
             self.map.get_unit(action.unit_id).in_progress = False
 
         # 4) end of episode check & clean up
         self.time += 1
         if self.time >= self.max_steps_per_game():
+            print('GAME OVER, draw')
             self.is_game_over = True
 
     def get_state(self, unit_id=None):

src/pycrorts3/game/map.py
@@ -1,5 +1,5 @@
 import os
-from typing import Optional
+from typing import Dict, Optional
 
 import numpy as np
 import pkg_resources
@@ -33,7 +33,7 @@ class Map:
                 i += 1
 
         # units
-        self.units = {}
+        self.units: Dict[int, Unit] = {}
         self.unit_map = np.zeros((self.height, self.width), dtype=np.uint8)
         for unit in map_data.rts_PhysicalGameState.units.rts_units_Unit:
             unit_id = int(unit['ID'])
@@ -46,11 +46,15 @@ class Map:
         return self.units[unit_id]
 
     def move_unit(self, unit_id, new_position):
-        assert unit_id in self.units
+        if unit_id not in self.units:
+            raise ValueError
+        # assert unit_id in self.units
         unit = self.units[unit_id]
         old_x, old_y = unit.position
         new_x, new_y = new_position
-        assert self.terrain[new_y, new_x] == 0 and self.unit_map[new_y, new_x] == 0
+        if not(self.terrain[new_y, new_x] == 0 and self.unit_map[new_y, new_x] == 0):
+            raise ValueError
+        # assert self.terrain[new_y, new_x] == 0 and self.unit_map[new_y, new_x] == 0
         self.unit_map[new_y, new_x], self.unit_map[old_y, old_x] = self.unit_map[old_y, old_x], 0
         unit.position = new_position
 
@@ -79,8 +83,10 @@ class Map:
 
         :param unit_id: The ID of the unit to remove.
         """
-        unit = self.units.pop(unit_id)
+        # unit = self.units.pop(unit_id)
+        unit = self.units[unit_id]
         self.unit_map[unit.y, unit.x] = 0
+        unit.position = Position(-1, -1)  # move them off the board so other units can occupy their former location
 
     def is_legal_action(self, action: Action) -> bool:
         """Check an action is consistent with game rules/state?
@@ -132,7 +138,11 @@ class Map:
         if unit_id is not None:
             assert unit_id in self.units
             unit = self.units[unit_id]
-            state[unit.y, unit.x] += len(UnitEncoding)
+            if not unit.is_dead():
+                state[unit.y, unit.x] += len(UnitEncoding)
+            for other in self.units.values():
+                if other.player_id == unit.player_id:
+                    state[other.y, other.x] += len(UnitEncoding)
         return state
 
     def _read_map_file(self, map_filename: str):